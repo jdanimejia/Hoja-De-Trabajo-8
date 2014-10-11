3e79ad3c4fcc3028cb9dbd5042959e4f24071def
Daniel <happy@Danny-2.local>
2014-10-10 21:42:40 -0600

    word set

---
+++ WordSet.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * Universidad del Valle de Guatemala
+ * @author  Diego Juarez    13280
+ *          Daniel Mejia    13271
+ *          Jose Sagastume  13217
+ * Algoritmos y estructura de datos
+ * Seccion: 30
+ *          
+ */
+public interface WordSet {
+
+	public void add(Word wordObject);
+	public Word get(Word word);
+	
+}
