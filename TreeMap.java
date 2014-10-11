f58cd8fbae01871a58159e19e29bb0ea5822b35b
Daniel <happy@Danny-2.local>
2014-10-10 21:40:53 -0600

    tree map
    map

---
+++ TreeMap.java
@@ -0,0 +1,30 @@
+/**
+ *
+ * Universidad del Valle de Guatemala
+ * @author  Diego Juarez    13280
+ *          Daniel Mejia    13271
+ *          Jose Sagastume  13217
+ * Algoritmos y estructura de datos
+ * Seccion: 30
+ *          
+ */
+
+import java.util.*;
+
+class TreeMap implements WordSet
+{
+        private ArrayList<Word> base;
+        
+        public TreeMap()
+        {
+                base = new ArrayList<Word>();
+        }
+        
+        public Word get(Word word){
+            return null;
+        }
+        
+        public void add(Word wordObject)
+        {
+        }
+}
